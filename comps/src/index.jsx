f3186c70e6b374d8bf891a49b633e51c0415fc4a
Tugrul Arabaci <rbctugrul@gmail.com>
2023-03-26 23:01:33 +0300

    comps project deleted

--- comps/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from './App';
-
-const el = document.getElementById("root");
-const root = ReactDOM.createRoot(el);
-
-root.render(<App />);
