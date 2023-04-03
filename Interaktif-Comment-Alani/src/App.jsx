c2b45ee544a414a9d6eee2b6deaa3c5cc052d3e4
Tugrul Arabaci <rbctugrul@gmail.com>
2023-04-03 18:31:07 +0300

    Interaktif Comment Alani files created

---
+++ Interaktif-Comment-Alani/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./styles/reset.scss";
+import "./styles/variables.scss";
+import "./styles/global.scss";
+
+function App() {
+  return <div>Interactive Comments</div>;
+}
+
+export default App;
