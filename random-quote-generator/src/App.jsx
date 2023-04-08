6f8799435a73875757ee36729dadc2ebbda7b0ee
Tugrul Arabaci <rbctugrul@gmail.com>
2023-04-09 01:13:53 +0300

    Random quote generator finished

--- random-quote-generator/src/App.jsx
+++ random-quote-generator/src/App.jsx
@@ -1,7 +1,32 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
 function App() {
+  const [quotes, setQuotes] = useState("");
+  const getQuote = () => {
+    fetch("https://type.fit/api/quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        let randomNum = Math.floor(Math.random() * data.length);
+        setQuotes(data[randomNum]);
+      });
+  };
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
   return (
     <div className="App">
-      App
+      <div className="quote">
+        <p>{quotes.text}</p>
+        <p style={{ fontWeight: 700 }}>{quotes.author}</p>
+        <div className="btnContainer">
+          <button onClick={getQuote} className="btn">
+            Get Quote
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
