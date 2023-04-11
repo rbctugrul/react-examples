fa30c96b9afc22b2e3b6aa61c8d971bf30e9ec57
Tugrul Arabaci <rbctugrul@gmail.com>
2023-04-11 15:56:34 +0300

    swimming places completed

---
+++ swimming-places/src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./App.css";
+import Categories from "./Categories";
+import items from "./data";
+import Menu from "./Menu";
+
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [activeCategory, setActiveCategory] = useState("");
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    setActiveCategory(category);
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main className="App">
+      <section className="holiday section">
+        <div className="title">
+          <h2>Cukurova Swimming Places</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories
+          categories={categories}
+          activeCategory={activeCategory}
+          filterItems={filterItems}
+        />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
