2875c8c932e8a59bc80a5ecee0a43c0b2efc99f3
Tugrul Arabaci <rbctugrul@gmail.com>
2023-04-12 23:38:21 +0300

    hava durumu's api's added

--- hava-durumu/src/App.jsx
+++ hava-durumu/src/App.jsx
@@ -1,9 +1,31 @@
-function App() {
-  return (
-    <div className="App">
-      <p>a</p>
-    </div>
-  );
-}
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { usePosition } from "use-position";
+
+const App = () => {
+  const [weather, setWeather] = useState();
+  const { latitude, longitude } = usePosition();
+
+  const getWeatherData = async (lat, lon) => {
+    const key = "2aa95feb659b6eab64769e1fc8a072cc";
+
+    try {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
+      );
+      setWeather(data);
+    } catch {
+      alert("Veri alinirken hata oluştu");
+    }
+  };
+
+  useEffect(() => {
+    latitude && longitude && getWeatherData(latitude, longitude);
+  }, [latitude, longitude]);
+
+  console.log(latitude, longitude, weather);
+
+  return <div>Hava Durumu</div>;
+};
 
 export default App;
