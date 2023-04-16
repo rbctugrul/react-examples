eef6d6b13121d0533af8727eac42978c89e06781
Tugrul Arabaci <rbctugrul@gmail.com>
2023-04-16 12:16:33 +0300

    rolling-dice completed

--- rolling-dice/src/components/Dice.jsx
+++ rolling-dice/src/components/Dice.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./dice.css";
 
-function Dice({ face }) {
-  return <i className={`dice fas fa-dice-${face}`}></i>;
+function Dice({ face, rolling }) {
+  return <i className={`dice fas fa-dice-${face} ${rolling && "shaking"}`}></i>;
 }
 
 export default Dice;
