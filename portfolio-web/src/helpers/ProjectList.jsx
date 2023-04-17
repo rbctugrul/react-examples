67c6bee2b4df687d43ea31a5680a5bbc09f7acf1
Tugrul Arabaci <rbctugrul@gmail.com>
2023-04-17 14:32:09 +0300

    Experience completed

---
+++ portfolio-web/src/helpers/ProjectList.jsx
@@ -0,0 +1,29 @@
+export const ProjectList = [
+  {
+    name: "Recuno.com",
+    image: "https://pbs.twimg.com/media/E7S3QliX0AAF6nN.jpg",
+  },
+  {
+    name: "77food.com",
+    image:
+      "https://localinsider.storage.googleapis.com/2022/07/best-food-delivery-apps-.jpeg",
+  },
+  {
+    name: "darksheets.com",
+    image: "https://cdn.wmaraci.com/nedir/forum.png",
+  },
+  {
+    name: "starbucks.com",
+    image:
+      "https://screenshots.webflow.com/sites/607d3f3b88d1c85f6691eff2/20210419114759_246d885cbcec8bebf7bb651f8e928c78.png",
+  },
+  {
+    name: "77food.com",
+    image:
+      "https://localinsider.storage.googleapis.com/2022/07/best-food-delivery-apps-.jpeg",
+  },
+  {
+    name: "darksheets.com",
+    image: "https://cdn.wmaraci.com/nedir/forum.png",
+  },
+];
--- portfolio-web/src/pages/Experience.jsx
+++ portfolio-web/src/pages/Experience.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { IoMdSchool } from "react-icons/io";
+import { MdBusinessCenter } from "react-icons/md";
+import { HiQuestionMarkCircle } from "react-icons/hi";
 
 function Experience() {
   return (
-    <div>Experience</div>
-  )
+    <div className="expContainer">
+      <div className="expElements">
+        <ol>
+          <li>
+            <IoMdSchool />
+            <h3>High School Graduation 2019</h3>
+            <p>Fatih lisesi</p>
+          </li>
+          <li>
+            <IoMdSchool />
+            <h3>University Graduation 2025</h3>
+            <p>Eskisehir University</p>
+          </li>
+          <li>
+            <MdBusinessCenter />
+            <h3>Software Engineer 2023</h3>
+            <p>Microsoft</p>
+          </li>
+          <li>
+            <HiQuestionMarkCircle />
+            <h3>...?</h3>
+            <p>?</p>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
 export default Experience
