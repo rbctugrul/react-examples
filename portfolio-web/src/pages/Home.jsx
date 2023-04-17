1f7c271be18d761eaf514e88b1403c954995a9e9
Tugrul Arabaci <rbctugrul@gmail.com>
2023-04-17 14:20:23 +0300

    Home completed

--- portfolio-web/src/pages/Home.jsx
+++ portfolio-web/src/pages/Home.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { FiMail } from "react-icons/fi";
+import { BsGithub } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home">
+      <div className="about">
+        <h2>Hello Bro</h2>
+        <div className="prompt">
+          <p>Software engineer</p>
+          <BsGithub />
+          <BsLinkedin />
+          <FiMail />
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Front-End</h2>
+            <span>Reactjs,Angular,Redux,Css</span>
+          </li>
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>Nodejs,Django,Spring</span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>Javascript,Java,Python</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
