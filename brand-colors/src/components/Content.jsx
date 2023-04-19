dcc28beb881c8aee0ff99fa624d6bdfe59d8d6ef
Tugrul Arabaci <rbctugrul@gmail.com>
2023-04-19 13:53:40 +0300

    download added

--- brand-colors/src/components/Content.jsx
+++ brand-colors/src/components/Content.jsx
@@ -3,18 +3,22 @@ import Search from "./Search";
 import Brand from "./Brand";
 import MainContext from "./MainContext";
 import LazyLoad from "react-lazyload";
+import Download from "./Download";
 
 function Content() {
-  const { brands } = useContext(MainContext);
+  const { brands,selectedBrands } = useContext(MainContext);
 
   return (
     <main className="content">
       <header className="header">
         <Search />
+        {selectedBrands.length !== 0 && <Download/> }
       </header>
       <section className="brands">
         {brands.map((brand) => (
-          <Brand brand={brand} />
+          <LazyLoad once={true} overflow={true} placeholder="Yükleniyor...">
+            <Brand brand={brand} />
+          </LazyLoad>
         ))}
       </section>
     </main>
