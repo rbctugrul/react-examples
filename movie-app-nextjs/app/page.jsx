c4b89307fb28454dde596d4b362fb2bd64e945b3
Tugrul Arabaci <rbctugrul@gmail.com>
2023-04-28 14:31:28 +0300

    movie app next deleted

--- movie-app-nextjs/app/page.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import HomeContainer from "@/containers/home";
-
-function HomePage() {
-  return <HomeContainer />;
-}
-
-export default HomePage;
--- movie-app-nextjs/containers/home/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function HomeContainer() {
-  return (
-    <div>HomeContainer</div>
-  )
-}
-
-export default HomeContainer
