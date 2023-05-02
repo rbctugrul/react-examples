1525278a82bb36d1fb9be5279af2ddacdb0ab8e8
Tugrul Arabaci <rbctugrul@gmail.com>
2023-05-02 12:00:08 +0300

    Search button added

--- food-app/src/components/Navbar.jsx
+++ food-app/src/components/Navbar.jsx
@@ -1,15 +1,29 @@
 import React from "react";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
 
 const Navbar = () => {
   return (
-    <div>
-      <div>
-        <AiOutlineMenu size={30} />
+    <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
+      {/* Left Side */}
+      <div className="flex items-center">
+        <div className="cursor-pointer">
+          <AiOutlineMenu size={30} />
+        </div>
+        <h1 className="text-2xl sm:text-3xl lg:text-4xl">
+          Best <span className="font-bold">Eats</span>
+        </h1>
+        <div className="hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]">
+          <p className="bg-black text-white rounded-full p-2">Deliveriy</p>
+          <p className="p-2">Pickup</p>
+        </div>
       </div>
-      <h1>
-        Best <span>Eats</span>
-      </h1>
+
+      {/* Search Input */}
+
+      <div className="bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]">
+        <AiOutlineSearch size={20} />
+        <input className="bg-transparent w-full focus:outline-none" type="text" placeholder="Search foods" />
+      </div>
     </div>
   );
 };
