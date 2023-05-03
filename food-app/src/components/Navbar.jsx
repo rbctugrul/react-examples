e942bb67edb7869f6edba05ec2382f6beb8b41e5
Tugrul Arabaci <rbctugrul@gmail.com>
2023-05-03 13:36:20 +0300

    side drawer menu completed

--- food-app/src/components/Navbar.jsx
+++ food-app/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   AiFillTag,
   AiOutlineClose,
@@ -11,11 +11,13 @@ import { FaUserFriends, FaWallet } from "react-icons/fa";
 import { MdFavorite, MdHelp } from "react-icons/md";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       {/* Left Side */}
       <div className="flex items-center">
-        <div className="cursor-pointer">
+        <div onClick={() => setNav(!nav)} className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl">
@@ -44,12 +46,19 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       {/**Overlay */}
-      <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+      {nav ? (
+        <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
+      ) : (
+        ""
+      )}
 
       {/**Side drawer menu */}
       <div
-        className="fixed top-0 left-0 w-[300px]
-      h-screen bg-white z-10 duration-300"
+        className={
+          nav
+            ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300"
+            : "fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"
+        }
       >
         <AiOutlineClose
           size={30}
