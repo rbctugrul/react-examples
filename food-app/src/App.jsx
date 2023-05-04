87b77f5abc16854d942e00501955f245e30ab4be
Tugrul Arabaci <rbctugrul@gmail.com>
2023-05-04 13:03:03 +0300

    Added images in HeadlineCards

--- food-app/src/App.jsx
+++ food-app/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/navbar";
 import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <HeadlineCards />
     </div>
   );
 }
--- food-app/src/components/HeadlineCards.jsx
+++ food-app/src/components/HeadlineCards.jsx
@@ -13,7 +13,11 @@ const HeadlineCards = () => {
             Order Now
           </button>
         </div>
-       
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.unsplash.com/photo-1613769049987-b31b641f25b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fGJyZWFrZmFzdHxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60"
+          alt="/"
+        />
       </div>
       {/* Card */}
       <div className="rounded-xl relative">
@@ -25,7 +29,11 @@ const HeadlineCards = () => {
             Order Now
           </button>
         </div>
-        
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.unsplash.com/photo-1544025162-d76694265947?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJicXxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60"
+          alt="/"
+        />
       </div>
       {/* Card */}
       <div className="rounded-xl relative">
@@ -39,7 +47,11 @@ const HeadlineCards = () => {
             Order Now
           </button>
         </div>
-        
+        <img
+          className="max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl"
+          src="https://images.unsplash.com/photo-1559715745-e1b33a271c8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fGRlc3NlcnR8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60"
+          alt="/"
+        />
       </div>
     </div>
   );
