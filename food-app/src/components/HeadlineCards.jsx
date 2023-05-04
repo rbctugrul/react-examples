f5510508ff66c41b10e42ec7918eab00ea7f03a1
Tugrul Arabaci <rbctugrul@gmail.com>
2023-05-04 13:00:41 +0300

    Added somthing in HeadlineCards

--- food-app/src/components/HeadlineCards.jsx
+++ food-app/src/components/HeadlineCards.jsx
@@ -1,7 +1,48 @@
 import React from "react";
 
 const HeadlineCards = () => {
-  return <div>HeadlineCards</div>;
+  return (
+    <div className="max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6">
+      {/* Card */}
+      <div className="rounded-xl relative">
+        {/* Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">Sun's Out, BOGO's Out</p>
+          <p className="px-2">Through 8/26</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+       
+      </div>
+      {/* Card */}
+      <div className="rounded-xl relative">
+        {/* Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">New Restaurants</p>
+          <p className="px-2">Added Daily</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        
+      </div>
+      {/* Card */}
+      <div className="rounded-xl relative">
+        {/* Overlay */}
+        <div className="absolute w-full h-full bg-black/50 rounded-xl text-white">
+          <p className="font-bold text-2xl px-2 pt-4">
+            We Deliver Desserts Too
+          </p>
+          <p className="px-2">Tasty Treats</p>
+          <button className="border-white bg-white text-black mx-2 absolute bottom-4">
+            Order Now
+          </button>
+        </div>
+        
+      </div>
+    </div>
+  );
 };
 
 export default HeadlineCards;
