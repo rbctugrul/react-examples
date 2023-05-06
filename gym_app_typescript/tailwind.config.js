56ee4faa9c1f5058adad472410494ff5a9924aa2
Tugrul Arabaci <rbctugrul@gmail.com>
2023-05-06 17:54:54 +0300

    prettier config created

--- gym_app_typescript/tailwind.config.js
+++ gym_app_typescript/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "gray-20": "#F8F4EB",
+        "gray-50": "#EFE6E6",
+        "gray-100": "#DFCCCC",
+        "gray-500": "#5E0000",
+        "primary-100": "#FFE1E0",
+        "primary-300": "#FFA6A3",
+        "primary-500": "#FF6B66",
+        "secondary-400": "#FFCD5B",
+        "secondary-500": "#FFC132",
+      }
+    },
   },
   plugins: [],
 };
+ 
