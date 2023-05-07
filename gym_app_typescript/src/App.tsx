13ca3419ee37fb47362acbe23a60429728ef2c6e
Tugrul Arabaci <rbctugrul@gmail.com>
2023-05-07 15:11:15 +0300

    index.tsx at navbar created

--- gym_app_typescript/src/App.tsx
+++ gym_app_typescript/src/App.tsx
@@ -1,5 +1,13 @@
+import Navbar from "./scenes/navbar";
+
+
+
 function App() {
-  return <div className="app bg-gray-20">App</div>;
+  return (
+    <div className="app bg-gray-20">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ gym_app_typescript/src/scenes/navbar/index.tsx
@@ -0,0 +1,13 @@
+
+import { useEffect, useState } from 'react'
+ 
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
