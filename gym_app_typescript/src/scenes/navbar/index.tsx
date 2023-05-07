abcc1848b0fcff64817d0acd50057cde1b66eb92
Tugrul Arabaci <rbctugrul@gmail.com>
2023-05-07 15:24:07 +0300

    Main navbar container created

--- gym_app_typescript/src/scenes/navbar/index.tsx
+++ gym_app_typescript/src/scenes/navbar/index.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Logo from "@/assets/Logo.png";
 
-import { useEffect, useState } from 'react'
- 
-
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const flexBetween = "flex items-center justify-between";
 
-export default Navbar
+  return (
+    <nav>
+      <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+        <div>
+          
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
